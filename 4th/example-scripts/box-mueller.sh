1b0a5cae128a08ec75e72c4864cee0ead4178872
Zebulun Arendsee <arendsee@iastate.edu>
2014-08-12 04:57:41 -0500

    initial commit

---
+++ 4th/example-scripts/box-mueller.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This example uses AWK in a context unrelated to text processing.
+
+# AWK implementation of the Box-Muller normal random number generator
+
+# Each transform uses two uniform random variables and outputs two normal
+# random variables
+
+# Two variables are passed to AWK 
+#    n - the number of random normal variables to output
+#    s - the initial random seed, see note below
+echo | awk -v n=$1 -v s=$RANDOM$RANDOM '
+    BEGIN {
+        pi = 4 * atan2(1,1)
+        srand(s)
+    }
+
+    function rnorm(pi){
+        r1 = rand()
+        r2 = rand()
+        a = sqrt(-2 * log(r1)) * cos(2 * pi * r2)
+        b = sqrt(-2 * log(r1)) * sin(2 * pi * r2)
+        return
+    }
+
+    END {
+        for (i=1; i <= n/2; i++){
+            rnorm(pi)
+            print a "\n" b
+        }
+    }
+'
+
+# $RANDOM environmental variable:
+#    The system collects noise which it feeds into a pool of true random bits.
+#    Each time $RANDOM is called, two bytes from this pool are returned and
+#    removed form the pool.
+#    If we did not provide AWK a random seed, it would seed its
+#    pseudorandom number generator with system time. Try running this script
+#    repeatedly: 
+# echo | awk 'BEGIN{srand()} {print rand()}'
+#    in my implementation of AWK (GNU AWK 4.0.1) calls witin the same second
+#    have the same output.
